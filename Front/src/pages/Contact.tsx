936e65cf2d0e33d36840077c8fcae04e32833ccc
florent.rve <florent.rve@gmail.com>
2024-05-16 13:08:44 +0400

    css mineur

--- Front/src/pages/Contact.tsx
+++ Front/src/pages/Contact.tsx
@@ -36,26 +36,26 @@ function Contact() {
       <Title title="Contact" />
       <div className="flex flex-col justify-center items-center mt-10">
         <div className="flex w-[80%] gap-10 justify-between">
-          <div className="bg-blue-300 p-10 w-[30%]">
+          <div className="shadow-xl flex flex-col gap-5 p-10 w-[30%]">
             <p className="text-xl font-bold mb-5">Contact Information</p>
 
-            <div className="flex gap-2">
+            <div className="flex items-center gap-2">
               <GiPositionMarker />
-              <p>Adresse</p>
+              <p>44 rue du muscle</p>
             </div>
 
-            <div className="flex gap-2">
+            <div className="flex items-center gap-2">
               <BsTelephone />
               <p>02 56 25 32 14</p>
             </div>
 
-            <div className="flex gap-2">
+            <div className="flex items-center gap-2">
               <CiMail />
-              <p>test@mail.fr</p>
+              <p>MuscleBuster@mail.fr</p>
             </div>
           </div>
 
-          <div className="p-10 bg-red-300 w-[70%]">
+          <div className="p-10 shadow-xl w-[70%]">
             <p className="text-xl font-bold mb-5">Envoyez nous un mail</p>
             <form
               className="flex flex-col gap-3"
@@ -65,7 +65,7 @@ function Contact() {
               <input
                 type="text"
                 placeholder="Nom"
-                className="w-full"
+                className="w-full border-blue-900 border-2 p-2 rounded-lg"
                 name="nom"
                 id="nom"
               />
@@ -74,21 +74,21 @@ function Contact() {
                 <input
                   type="text"
                   placeholder="Email"
-                  className="w-1/2"
+                  className="w-1/2 border-blue-900 border-2 p-2 rounded-lg"
                   name="mail"
                   id="mail"
                 />
                 <input
                   type="text"
                   placeholder="Sujet"
-                  className="w-1/2"
+                  className="w-1/2 border-blue-900 border-2 p-2 rounded-lg"
                   name="sujet"
                   id="sujet"
                 />
               </div>
 
               <textarea
-                className="w-full"
+                className="w-full border-blue-900 border-2 p-2 rounded-lg"
                 placeholder="Message"
                 name="message"
                 id="message"
@@ -96,7 +96,7 @@ function Contact() {
 
               <button
                 type="submit"
-                className="bg-orange-400 text-white font-bold p-2 rounded-2xl w-20 self-end"
+                className="bg-orange-400 text-white font-bold p-2 rounded-2xl w-20 self-end hover:scale-125"
               >
                 Envoyer
               </button>
